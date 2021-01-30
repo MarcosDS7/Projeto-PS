e6c699dbe33b3d494dc97c9bc7172ccee36436bc
Marcos Vinícius Rodrigues de Souza <marcossouza.0ds@gmail.com>
2021-01-30 18:06:08 -0300

    Finalização

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
+const openMenu = document.getElementById("openMenu")
+const closeMenu = document.getElementById("closeMenu")
+const itemSv = document.getElementById("service")
+const itemQm = document.getElementById("WhoWeAre")
+const itemCt = document.getElementById("contact")
+const form = document.getElementById("form")
 
-const openMenu = () =>{
-$("#Modalmenu").modal("show")
-}
-
-const closeMenu = () =>{
-  $("#Modalmenu").modal("hide")
-  }
   
+ openMenu.addEventListener("click", () =>{
+  $("#Modalmenu").modal("show")
+});
+closeMenu.addEventListener("click", () =>{
+  $("#Modalmenu").modal("hide")
+});
+
+itemSv.addEventListener("click",  () =>{
+  $("#Modalmenu").modal("hide")
+});
+
+itemQm.addEventListener("click",  () =>{
+  $("#Modalmenu").modal("hide")
+});
+
+itemCt.addEventListener("click",  () =>{
+  $("#Modalmenu").modal("hide")
+});
+
+form.addEventListener("submit" ,  (event) =>{
+event.preventDefault()
+
+})
